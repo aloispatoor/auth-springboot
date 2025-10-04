4aab22310aa85e9331d7392fc71a0e26f59faf07
Alois <palois89@posteo.net>
2025-10-04 12:53:26 +0200

    first commit

---
+++ src/main/java/com/auth/AuthSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSpringbootApplication.class, args);
+	}
+
+}
